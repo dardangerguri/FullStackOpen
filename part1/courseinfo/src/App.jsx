d2c0356dd0b5883931b8ae3204f3d8aa8d7af960
Dardan <dardangerguri@outlook.com>
2025-02-08 17:31:58 +0200

    ex1.3-CourseInformation: Refactor App component to use objects for the variable definitions
    -Replace the individual variables for parts and exercises as objects.
    -Modified the return statement of the App component to support object structure.
    -Updated the Part component to work with the new structure.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -22,7 +22,7 @@ const Part = (props) => {
   console.log(props)
   return (
     <div>
-      <p>{props.content.part} {props.content.exercises}</p>
+      <p>{props.content.name} {props.content.exercises}</p>
     </div>
   )
 }
@@ -38,22 +38,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content content={[
-        {part: part1, exercises: exercises1},
-        {part: part2, exercises: exercises2},
-        {part: part3, exercises: exercises3}
-      ]} />
-      <Total exercisesTotal={exercises1 + exercises2 + exercises3}/>
+      <Content content={[part1, part2, part3]} />
+      <Total exercisesTotal={part1.exercises + part2.exercises + part3.exercises}/>
     </div>
   )
 }
