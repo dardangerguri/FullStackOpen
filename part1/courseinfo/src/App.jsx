7e5ffbc03a09c003213aded035172ca08b916f0f
Dardan <dardangerguri@outlook.com>
2025-02-08 15:28:23 +0200

    ex1.1-CourseInformation: Initialize app and refactor the app.
    -Initialized the app with Vite.
    -Modified the main.jsx and App.jsx as required by the subject.
    -Removed the App.css and index.css.
    -Split the App component (code from the subject) into Header, Content, Total components and passed the necessary data.

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,52 @@
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>{props.content[0].part} {props.content[0].exercises}</p>
+      <p>{props.content[1].part} {props.content[1].exercises}</p>
+      <p>{props.content[2].part} {props.content[2].exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Number of exercises {props.exercisesTotal}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content content={[
+        {part: part1, exercises: exercises1},
+        {part: part2, exercises: exercises2},
+        {part: part3, exercises: exercises3}
+      ]} />
+      <Total exercisesTotal={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App
