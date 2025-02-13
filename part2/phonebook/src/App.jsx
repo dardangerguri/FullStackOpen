60ca67f171308eb612892157b44441e16100f418
Dardan <dardangerguri@outlook.com>
2025-02-13 15:42:48 +0200

    ex2.9-Phonebook: Add search filter to the phonebook
    -Added a search input field that allows users to filter contacts by name.
    -The filtering logic is case insensitive.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterText, setFilterText] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -42,9 +43,24 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterTextChange = (event) => {
+    console.log(event.target.value)
+    setFilterText(event.target.value)
+  }
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filterText.toLowerCase())
+  )
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with<input value={filterText} onChange={handleFilterTextChange}/>
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -57,7 +73,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {filteredPersons.map((person) => (
         <div key={person.name}>{person.name} {person.number}</div>
       ))}
     </div>
