5605107f0394c565adafd6ffc88663f1a43fb66d
Dardan <dardangerguri@outlook.com>
2025-02-14 14:25:00 +0200

    ex2.12-Phonebook: Save the new contacts to the server
    -New contacts are now send to the server to be regristered.
    -Used axios to make the POST request.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -38,7 +38,11 @@ const App = () => {
       number: newNumber
     }
 
-    setPersons(persons.concat(personObject))
+    axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(personObject))
+      })
     setNewName('')
     setNewNumber('')
   }
