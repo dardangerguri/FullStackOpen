49f8fd8e3e7940c4c1416de8e62ffa92493cd35b
Dardan <dardangerguri@outlook.com>
2025-03-25 23:01:37 +0200

    ex4.5-BlogList: Implement favoriteBlog function with unit tests

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,23 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+  const blog = blogs.find(blog => blog.likes === maxLikes)
+
+  return {
+    title: blog.title,
+    author: blog.author,
+    likes: blog.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
