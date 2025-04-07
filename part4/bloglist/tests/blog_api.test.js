40760b7ebc703cfbafe9daff60ad59fd1cc12922
Dardan <dardangerguri@outlook.com>
2025-04-07 17:26:46 +0300

    ex4.8-BlogList: Add GET request test using SuperTest and refactor route handler to async/await
    -Implemented a test using SuperTest to make a GET request to the /api/blogs endpoint.
    -Verified that the blog list application returns the correct amount of blog posts in JSON format.
    -Refactored the route handler to use async/await syntax.
    -Added a helper test function to manage blog data better for readibility.

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,35 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are two blogs', async () => {
+  const response = await helper.blogsInDb()
+
+  assert.strictEqual(response.length, helper.initialBlogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
