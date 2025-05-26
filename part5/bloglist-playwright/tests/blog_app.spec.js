381d784582847174f1b185c500446e101f84210a
Dardan <dardangerguri@outlook.com>
2025-05-26 17:07:12 +0300

    ex5.17-BlogList: Setup Playwright and add initial E2E test for login form

---
+++ part5/bloglist-playwright/tests/blog_app.spec.js
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Blog App', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('log in to application')).toBeVisible()
+
+    await expect(page.getByText('username')).toBeVisible()
+    await expect(page.getByText('password')).toBeVisible()
+
+    await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+})
