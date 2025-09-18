247e978bb72b1747a3b710af2011b82a1d364f9a
Dardan <dardangerguri@outlook.com>
2025-09-18 18:36:11 +0300

    ex8.11-Library: Add ability to set and update author birth year

--- part8/library-frontend/src/components/Authors.jsx
+++ part8/library-frontend/src/components/Authors.jsx
@@ -1,8 +1,16 @@
-import { useQuery } from '@apollo/client'
-import { ALL_AUTHORS } from '../queries'
+import { useState } from 'react'
+import { useMutation, useQuery } from '@apollo/client'
+import { ALL_AUTHORS, EDIT_BIRTHYEAR } from '../queries'
 
 const Authors = (props) => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
   const result = useQuery(ALL_AUTHORS)
+
+  const [ editBirthyear ] = useMutation(EDIT_BIRTHYEAR, {
+    refetchQueries: [ { query: ALL_AUTHORS } ]
+  })
 
   if (!props.show) {
     return null
@@ -17,6 +25,29 @@ const Authors = (props) => {
   }
 
   const authors = result.data.allAuthors
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    console.log('update author...')
+
+    const bornInt = Number(born)
+    if (isNaN(bornInt)) {
+      console.log('Born year must be a number')
+      return
+    }
+
+    const currentYear = new Date().getFullYear()
+    if (bornInt < 0 || bornInt > currentYear) {
+      console.log(`Born year must be between 0 and ${currentYear}`)
+      return
+    }
+
+    editBirthyear({ variables: { name, setBornTo: bornInt } })
+
+    setName('')
+    setBorn('')
+  }
 
   return (
     <div>
@@ -37,6 +68,16 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          name <input value={name} onChange={({ target }) => setName(target.value)} />
+        </div>
+        <div>
+          born <input value={born} onChange={({ target }) => setBorn(target.value)} />
+        </div>
+        <button type='submit'>update author</button>
+      </form>
     </div>
   )
 }
