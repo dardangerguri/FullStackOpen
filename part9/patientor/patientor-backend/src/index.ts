33280493adacabb98f0ff6b3b3a1ffd7270e3388
Dardan <dardangerguri@outlook.com>
2025-10-15 17:52:59 +0300

    ex9.10&ex9.11-Patientor: Implement Patientor backend endpoints for diagnoses and patients

--- part9/patientor/patientor-backend/src/index.ts
+++ part9/patientor/patientor-backend/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import diagnosesRouter from './routes/diagnoses';
+import patientsRouter from './routes/patients';
 const app = express();
 import cors from 'cors';
 
@@ -11,6 +13,9 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
+app.use('/api/patients', patientsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
